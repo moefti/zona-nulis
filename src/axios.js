90587d21385686ec5b7f84d2aba9c4e74d134509
moefti <rizalmuftinugraha@gmail.com>
2020-12-28 20:40:40 +0700

    impruv clientside

--- src/axios.js
+++ src/axios.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
-let instance;
-if (process.env.NODE_ENV === "development") {
-  instance = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
-    withCredentials: true,
-  });
-} else if (process.env.NODE_ENV === "production") {
-  instance = axios.create({
-    baseURL: "https://zona-nulis-api.herokuapp.com/api/v1",
-    withCredentials: true,
-  });
-}
+const instance = axios.create({
+  baseURL: "https://zona-nulis-api.herokuapp.com/api/v1",
+  withCredentials: true,
+});
 
 export default instance;
